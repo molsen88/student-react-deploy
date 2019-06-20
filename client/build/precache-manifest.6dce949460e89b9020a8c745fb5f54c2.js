0c6af00b8fae6a6b1e152bb0a1fff20eff223a47
Michael Olsen <michaelolsen@Michaels-MacBook-Air.local>
2019-06-19 21:22:44 -0600

    test react heroku 1

---
+++ client/build/precache-manifest.6dce949460e89b9020a8c745fb5f54c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b293bf656309eef5ac5",
+    "url": "/static/css/main.5621862c.chunk.css"
+  },
+  {
+    "revision": "1b293bf656309eef5ac5",
+    "url": "/static/js/main.3a97adf6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "08e60256e97de3e5b743",
+    "url": "/static/js/2.8006c6cb.chunk.js"
+  },
+  {
+    "revision": "1109b0925e44a6b06fd539344b48af0a",
+    "url": "/index.html"
+  }
+];
